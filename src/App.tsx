1b4f52bbbe48d6d110fac59f461adca9f40e1865
Roshansharmash <sharmash495@gmail.com>
2025-05-02 21:26:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -18,9 +17,14 @@ import NotFound from "./pages/NotFound";
 import Appointments from "./pages/Appointments";
 import FindDoctor from "./pages/FindDoctor";
 import MedicalRecords from "./pages/MedicalRecords";
-import Profile from "./pages/Profile";
 import AppointmentDetails from "./pages/AppointmentDetails";
 import Prescriptions from "./pages/Prescriptions";
+
+// Import new doctor pages
+import DoctorPrescriptions from "./pages/DoctorPrescriptions";
+import DoctorSchedule from "./pages/DoctorSchedule";
+import DoctorMessages from "./pages/DoctorMessages";
+import DoctorPatientRecords from "./pages/DoctorPatientRecords";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -56,8 +60,14 @@ const App = () => {
               <Route path="/appointment/:id" element={<AppointmentDetails />} />
               <Route path="/find-doctor" element={<FindDoctor />} />
               <Route path="/medical-records" element={<MedicalRecords />} />
-              <Route path="/profile" element={<Profile />} />
               <Route path="/prescriptions" element={<Prescriptions />} />
+              
+              {/* New Doctor Routes */}
+              <Route path="/doctor/prescriptions" element={<DoctorPrescriptions />} />
+              <Route path="/doctor/schedule" element={<DoctorSchedule />} />
+              <Route path="/doctor/messages" element={<DoctorMessages />} />
+              <Route path="/doctor/patient-records" element={<DoctorPatientRecords />} />
+              
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
